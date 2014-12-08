7200c18227c23aa21762c1fe170dc1918a62a760
Xenocide007 <shrackx@gmail.com>
2014-12-08 09:09:35 -0700

    Fixed Hard Coding
    the coding has been fixed!

--- pygame_stuff.py
+++ pygame_stuff.py
@@ -6,11 +6,17 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 
-gameDisplay = pygame.display.set_mode((800, 600))
+displayWidth = 800
+dispalyHeight = 600
+speed = 5
+rektWidth = 10
+rektHeight = 10
+
+gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
 pygame.display.set_caption("Stuff")
 
-lead_x = 400
-lead_y = 300
+lead_x = displayWidth/2
+lead_y = displayHeight/2
 lead_x_change = 0
 lead_y_change = 0
 
@@ -25,31 +31,31 @@ while not gameExit:
 			gameExit = True
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
-				lead_x_change = -5
+				lead_x_change = -speed
 				lead_y_change = 0
 			if event.key == pygame.K_RIGHT:
-				lead_x_change = 5
+				lead_x_change = speed
 				lead_y_change = 0
 			if event.key == pygame.K_UP:
-				lead_y_change = -5
+				lead_y_change = -speed
 				lead_x_change = 0
 			if event.key == pygame.K_DOWN:
-				lead_y_change = 5
+				lead_y_change = speed
 				lead_x_change = 0
-	if lead_x >= 800:
-		lead_x_change = -5
+	if lead_x >= displayWidth:
+		lead_x_change = -speed
 	if lead_x <= 0:
-		lead_x_change = 5
-	if lead_y >= 600:
-		lead_y_change = -5
+		lead_x_change = speed
+	if lead_y >= displayHeight:
+		lead_y_change = -speed
 	if lead_y <= 0:
-		lead_y_change = 5		
+		lead_y_change = speed		
 
 
 	lead_x += lead_x_change
 	lead_y += lead_y_change
 	gameDisplay.fill(white)
-	pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
+	pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, rektWidth, rektHeight])
 	pygame.display.update()
 
 	clock.tick(30)
