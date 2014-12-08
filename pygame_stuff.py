3b46a8716e5f3f222fd9c8ab8deedaf0aa96e4e8
Xenocide007 <shrackx@gmail.com>
2014-12-08 09:37:13 -0700

    asdfsad

--- pygame_stuff.py
+++ pygame_stuff.py
@@ -1,5 +1,6 @@
-import pygame
+import pygame, time
 from pygame.locals import *
+
 pygame.init()
 
 white = (255,255,255)
@@ -11,8 +12,10 @@ displayHeight = 600
 speed = 5
 rektWidth = 10
 rektHeight = 10
+fontSize = 25
 
 gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
+gameDisplay.fill(white)
 pygame.display.set_caption("Stuff")
 
 lead_x = displayWidth/2
@@ -21,6 +24,15 @@ lead_x_change = 0
 lead_y_change = 0
 
 clock = pygame.time.Clock()
+
+font = pygame.font.SysFont(None, fontSize)
+
+def message_to_screen(msg, color):
+	screen_text = font.render(msg, True, color)
+	gameDisplay.blit(screen_text, [displayWidth/2, displayHeight/2])
+
+
+
 
 
 gameExit = False
@@ -44,17 +56,20 @@ while not gameExit:
 				lead_x_change = 0
 	if lead_x >= displayWidth:
 		lead_x_change = -speed
+		message_to_screen("boing", red)
 	if lead_x <= 0:
 		lead_x_change = speed
+		message_to_screen("boing", red)
 	if lead_y >= displayHeight:
 		lead_y_change = -speed
+		message_to_screen("boing", red)
 	if lead_y <= 0:
-		lead_y_change = speed		
+		lead_y_change = speed
+		message_to_screen("boing", red)		
 
 
 	lead_x += lead_x_change
 	lead_y += lead_y_change
-	gameDisplay.fill(white)
 	pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, rektWidth, rektHeight])
 	pygame.display.update()
 
