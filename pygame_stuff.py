5825cdaa2e79b2bab33d0f5cb63f1c7243f05982
Xenocide007 <shrackx@gmail.com>
2014-12-10 09:35:37 -0700

    asdlkjfdasljk

--- pygame_stuff.py
+++ pygame_stuff.py
@@ -13,9 +13,10 @@ green = (0, 155, 0)
 display_width = 800
 display_height  = 600
 
+speed = 10
+
+
 snakeList = []
-
-
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Slither')
@@ -47,6 +48,8 @@ def gameLoop():
 
     lead_x_change = 0
     lead_y_change = 0
+	
+    snakeLength = 1
 
     randAppleX = round(random.randrange(0, display_width-block_size)/10.0)*10.0
     randAppleY = round(random.randrange(0, display_height-block_size)/10.0)*10.0
@@ -72,16 +75,16 @@ def gameLoop():
                 gameExit = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    lead_x_change = -block_size
+                    lead_x_change = -speed
                     lead_y_change = 0
                 elif event.key == pygame.K_RIGHT:
-                    lead_x_change = block_size
+                    lead_x_change = speed
                     lead_y_change = 0
                 elif event.key == pygame.K_UP:
-                    lead_y_change = -block_size
+                    lead_y_change = -speed
                     lead_x_change = 0
                 elif event.key == pygame.K_DOWN:
-                    lead_y_change = block_size
+                    lead_y_change = speed
                     lead_x_change = 0
 
         if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
@@ -97,13 +100,21 @@ def gameLoop():
         snakeHead.append(lead_x)
         snakeHead.append(lead_y)
         snakeList.append(snakeHead)
-        snake(block_size, snakeList)
+        if len(snakeList) > snakeLength:
+        	del snakeList[0]
+
+        for eachSegment in snakeList[:-1]:
+        	if eachSegment == snakeHead:
+        		gameOver = True
+
+
         snake(block_size, snakeList)
         pygame.display.update()
 
         if lead_x == randAppleX and lead_y == randAppleY:
         	randAppleX = round(random.randrange(0, display_width-block_size)/10.0)*10.0
     		randAppleY = round(random.randrange(0, display_height-block_size)/10.0)*10.0
+    		snakeLength += 1
 
         clock.tick(FPS)
         
